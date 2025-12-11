5da22267a42f9068ba8eacc2ca7a91dd5fde7471
Leon1owens <leon.owens@students.plymouth.ac.uk>
2025-12-11 14:41:52 +0000

    added ways to store info on current weight number of competitions and number of  hours of private coaching

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 /* Refer to the README.md for instructions on what you need to do in this project */
 let form = document.querySelector("#form");
-form.addEventListener("submit", function () {
-  console.log("Form submitted");
-});
+if (!form) {
+  console.error("Form not found");
+} else {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let athleteName = document.querySelector("#athlete-name").value;
+
+    let competitionsEntered = document.querySelector(
+      "#competitions-entered"
+    ).value;
+
+    let privateCoachingHours = document.querySelector(
+      "#private-coaching-hours"
+    ).value;
+
+    let currentWeight = document.querySelector("#current-weight").value;
+    console.log(
+      athleteName,
+      currentWeight,
+      competitionsEntered,
+      privateCoachingHours
+    );
+  });
+}
