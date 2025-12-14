d60ac940d32572c74f54d9f34a8cfdfce3576280
Leon1owens <leon.owens@students.plymouth.ac.uk>
2025-12-14 16:03:21 +0000

    fixed training plan being undefined in the console log

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ if (!form) {
 } else {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    let errors = {};
+
+  // Helper function to add error messages
+  function addError(field, message) {
+    if (!errors[field]) {
+      errors[field] = { messages: [] };
+    }
+    errors[field].messages.push(message);
+  }
+
     let athleteName = document.querySelector("#athlete-name").value;
 
     let competitionsEntered = document.querySelector(
@@ -22,14 +33,24 @@ if (!form) {
     );
 
     if (athleteName === "") {
-      alert("Athlete Name is required");
-      return;
+    addError("athleteName", "Please enter your name address.");
     }
     if (isNaN(currentWeight) || currentWeight <= 1) {
-      alert("Current Weight is required");
-      return;
+    addError("currentWeight", "Please enter your weight address.");
     }
+      for (let field in errors) {
+    let inputElement = document.querySelector(`#${field}`);
+    let labelElement = document.querySelector(`label[for=${field}]`);
+    if (inputElement) {
+      inputElement.classList.add("error-input");
+    }
+    if (labelElement) {
+      labelElement.classList.add("error-label");
+    }
+  }
 
+
+    console.log({ errors });
     console.log({
       athleteName,
       currentWeight,
